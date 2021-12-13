fede7f3ce6a4e003bfe0c8a308ac4ac2ec7e1237
Kevin Cenerino <cenerinok@gmail.com>
2021-12-13 22:56:58 +0100

    fix 1

--- script.js
+++ script.js
@@ -2,33 +2,33 @@ const crono = document.getElementById('crono')
 const start = document.getElementById('start')
 const stop = document.getElementById('stop')
 const howLong = document.getElementById('howLong')
+
 let i = 30
 const audio =  new Audio('bip.mp3')
 
+howLong.addEventListener('change', (e)=> {
+ i = (e.target.value);
+ crono.innerHTML = i
+})
 
 const countDown = () => {
-
-
+    
+    
     stop.addEventListener('click', () => {
-        clearInterval(interval)
-       })
-       howLong.addEventListener('change', (e)=> {
-        i = (e.target.value);
         clearInterval(interval)
     })
     const interval =  setInterval(() => {
         if(i > 0){
- i = i - 1
- crono.innerHTML = i ;
- console.log(i);
-    }else{
-        i = 30
-        audio.play()
-
-    }}
-    , 1000)
-  
-
+            i = i - 1
+            crono.innerHTML = i ;
+        }else{
+            i = 30
+            audio.play()
+            
+        }}
+        , 1000)
+        
+    
 }
 start.addEventListener('click', countDown)
 
